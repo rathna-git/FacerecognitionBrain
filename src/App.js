a7a9db857dffd0a583f331d72ffa164c8d2ee2e4
Rathna Ganjigunta <rathna.ganjigunta@gmail.com>
2020-01-17 16:34:13 -0800

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const particlesOptions = {
+  	particles: {
+      number:{
+        value: 80,
+        density: {
+          enable: true,
+          value_area: 800
+        }
+      }
+    },
+    color: {
+      value: '#fff'
+    },
+    line_linked: {
+      opacity: 1
+    }
+}
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+      <Particles className="particles"
+            params={particlesOptions}
+          />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {// <FaceRecognition />}
+      }
+      </div>
+    );
+  }
 }
 
 export default App;
